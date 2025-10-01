fa2c59de637fe8879477f7c391be042d8bd2cd73
Carlos <curiel@facturame.org>
2025-10-01 15:47:42 -0600

    fix: Usar DATABASE_URL para la conexión pgsql

--- config/database.php
+++ config/database.php
@@ -49,19 +49,19 @@ return [
         ],
 
         'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => 'public',
-            'sslmode' => 'prefer',
-        ],
+    'driver'         => 'pgsql',
+    'url'            => env('DATABASE_URL'), // <-- LA CLAVE ES ESTA LÍNEA
+    'host'           => env('DB_HOST', '127.0.0.1'),
+    'port'           => env('DB_PORT', '5432'),
+    'database'       => env('DB_DATABASE', 'laravel'),
+    'username'       => env('DB_USERNAME', 'root'),
+    'password'       => env('DB_PASSWORD', ''),
+    'charset'        => 'utf8',
+    'prefix'         => '',
+    'prefix_indexes' => true,
+    'search_path'    => 'public',
+    'sslmode'        => 'prefer',
+],
         
         // <<< INICIO DE LA CORRECCIÓN >>>
         'tenant' => [
