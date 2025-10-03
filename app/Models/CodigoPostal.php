74e2a20d63d92df99888e112a4e16f05e3bb8474
Carlos <curiel@facturame.org>
2025-10-03 12:36:30 -0600

    Siempre que busque un CP conectarse BD central

--- app/Models/CodigoPostal.php
+++ app/Models/CodigoPostal.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CodigoPostal extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * La conexión de base de datos que debe ser utilizada por el modelo.
+     * Forzamos a que siempre use la conexión 'pgsql' (la central/landlord).
+     *
+     * @var string
+     */
+    protected $connection = 'pgsql';
+    
+    // Si no tienes un $fillable, es bueno añadirlo.
+    protected $fillable = [
+        'codigo_postal',
+        'colonia',
+        'municipio',
+        'estado',
+    ];
 }
