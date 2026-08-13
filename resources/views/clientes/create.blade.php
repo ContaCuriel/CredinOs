a8d2ce871d3da01bc8a64c9ec556648aea56906c
Carlos <curiel@facturame.org>
2026-08-13 09:59:27 -0600

    Arreglando modulo clientes

--- resources/views/clientes/create.blade.php
+++ resources/views/clientes/create.blade.php
@@ -16,6 +16,12 @@
                                 <li>{{ $error }}</li>
                             @endforeach
                         </ul>
+                        @if (session('error'))
+                    <div class="alert alert-danger alert-dismissible fade show border-0 shadow-sm mt-3" role="alert">
+                        <i class="bi bi-exclamation-triangle-fill me-2"></i> {{ session('error') }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                @endif
                         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                     </div>
                 @endif
