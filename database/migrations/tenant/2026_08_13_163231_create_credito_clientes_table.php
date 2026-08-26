75c6409525d479c0622cdc7e6a1f69ab8eb51b25
Carlos <curiel@facturame.org>
2026-08-26 14:08:29 -0600

    Agregando UNIVERSO/SMARS solucionando problemas de migraciones2

--- database/migrations/tenant/2026_08_13_163231_create_credito_clientes_table.php
+++ database/migrations/tenant/2026_08_13_163231_create_credito_clientes_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             monto_individual DECIMAL(12,2) DEFAULT 0.00,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            CONSTRAINT fk_cc_credito FOREIGN KEY (credito_id) REFERENCES \"$schema\".creditos (id) ON DELETE CASCADE,
+            CONSTRAINT fk_cc_credito FOREIGN KEY (credito_id) REFERENCES \"$schema\".creditos (id_credito) ON DELETE CASCADE,
             CONSTRAINT fk_cc_cliente FOREIGN KEY (cliente_id) REFERENCES \"$schema\".clientes (id_cliente) ON DELETE CASCADE
         )";
         DB::connection('tenant')->statement($sql);
